157c7cab0a91969973e223cd70ee5a53cc6b322e
marxlima <marxmaciel@gmail.com>
2024-07-25 19:33:40 -0300

    start layout

---
+++ resources/js/Layouts/Layout.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <header>
+            <nav>
+
+                <div>
+                    <a href="/">Home</a>
+                    <a href="/about">About</a>
+
+                </div>
+            </nav>
+        </header>
+
+        <main>
+           <slot/>
+        </main>
+    </div>
+</template>
--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,6 +1,12 @@
+<script>
+import Layout from '../Layouts/Layout.vue';
+defineOptions({ layout: Layout })
+
+</script>
+
+
 <template>
-    <div>
+   
         <h1>Hello</h1>
-    </div>
 
 </template>
