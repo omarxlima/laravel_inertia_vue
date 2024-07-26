223e61cb36ec59106ef7cfbcbc19c35d5b3844ef
marxlima <marxmaciel@gmail.com>
2024-07-26 19:41:51 -0300

    mudança no auth

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,26 +1,33 @@
 <?php
 
+use App\Http\Middleware\AdminMidleware;
 use App\Http\Middleware\HandleInertiaRequests;
+use App\Http\Middleware\redirectAdminMidleware;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->alias([
+            'admin' => AdminMidleware::class,
+            'redirectAdminMiddleware' => redirectAdminMidleware::class,
+        ]);
         $middleware->web(append: [
             \App\Http\Middleware\HandleInertiaRequests::class,
             \Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets::class,
         ]);
-
+        
         $middleware->web(append: [
             HandleInertiaRequests::class,
         ]);
     })
+      
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })->create();
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,13 @@
 <?php
 
+use App\Http\Controllers\Admin\AdminAuthController;
+use App\Http\Controllers\Admin\AdminController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
+//Route users
 Route::get('/', function () {
     return Inertia::render('Welcome', [
         'canLogin' => Route::has('login'),
@@ -23,5 +26,20 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+//end Route Users
+
+Route::group(['prefix' => 'admin', 'middleware' => 'redirectAdminMiddleware'], function(){
+    Route::get('login', [AdminAuthController::class, 'showLoginForm'])->name('admin.login');
+    Route::post('login', [AdminAuthController::class, 'login'])->name('admin.login.post');
+    Route::post('logout', [AdminAuthController::class, 'logout'])->name('admin.logout');
+
+});
+
+// //Routes Admin
+Route::middleware(['auth', 'admin'])->prefix('admin')->group(function(){
+    Route::get('/dashboard', [AdminController::class, 'index'])->name('admin.dashboard');
+});
+// //End Routes Admin
 
 require __DIR__.'/auth.php';
